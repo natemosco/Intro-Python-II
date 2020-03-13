00499b7d4b405620cdfefd4bdb9654a83b132543
Nathaniel Mosco <natemosco@gmail.com>
2020-03-13 02:50:26 -0400

    follow along with brady initiate classes and make a player object

--- src/player.py
+++ src/player.py
@@ -1,2 +1,7 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, starting_location):
+        self.name = name
+        self.current_location = starting_location
